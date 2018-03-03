e3c538ae1578d54d3b6e973684a3a8d3ad3e29c3
Nick Wilson <logikfx@gmail.com>
2018-03-02 22:24:06 -0500

    learning basic features of cargo, Rusts package manager and build tool

---
+++ hello_cargo/target/debug/deps/hello_cargo-1ed2f9e6fa30ed7e.d
@@ -0,0 +1,5 @@
+/Users/nickwilson/Desktop/rust/hello_cargo/target/debug/deps/hello_cargo-1ed2f9e6fa30ed7e: src/main.rs
+
+/Users/nickwilson/Desktop/rust/hello_cargo/target/debug/deps/hello_cargo-1ed2f9e6fa30ed7e.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/nickwilson/Desktop/rust/hello_cargo/target/debug/hello_cargo.dSYM: /Users/nickwilson/Desktop/rust/hello_cargo/src/main.rs
