b35f862a4cba395c7913a95d771f7cc73b273264
Nick Wilson <logikfx@gmail.com>
2018-03-02 22:02:45 -0500

    Made a simple hello world program

---
+++ hello.rs
@@ -0,0 +1,13 @@
+// hello.rs
+// this is a line comment in Rust
+
+/// # Doc comment in Rust
+/// supports Markdown notation
+/// This program simply prints Hello, world!
+
+fn main() {
+    // the !, means we are calling a macro
+    println!("Hello, world!");
+}
+
+
